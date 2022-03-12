69d2b58ccdd169aae62765961cb0c7f8a54134b1
Vianney PETIT <vianneypetit@gmail.com>
2022-03-12 01:47:17 +0100

    Tweak heartbeat interval

--- src/js/ajax_pj.js
+++ src/js/ajax_pj.js
@@ -14,14 +14,16 @@ var update_screen = function() {
                         success: function(html, code){
                             $('#character-wrapper').html(html);
                             $('#loader').removeClass("active");
-                        }
+                        },
+                        timeout: 5000
                     });
                 }
                 setTimeout(update_screen, 8000 + Math.floor(Math.random() * 2000));
             },
             error: function() {
                 setTimeout(update_screen, 24000 + Math.floor(Math.random() * 6000));
-            }
+            },
+            timeout: 5000
         });
     }
 };
